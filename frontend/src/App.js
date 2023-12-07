a55b91ff93f7159f02f6c2d9a85edfb4a461f767
Smritiiiii <85865497+Smritiiiii@users.noreply.github.com>
2023-12-07 06:49:38 +0000

    integrated chat box on route /chat-box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -97,9 +97,6 @@
 
 // export default App;
 
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
 import Login from "./components/login/login";
@@ -111,8 +108,9 @@ import Achievements from "./components/achievements/achievements";
 import HomePage from "./components/homepage/homepage";
 import AdminDashboard from "./components/adminDashboard";
 import AdminInfo from "./components/admininfo";
-import StudentForm from "./components/studentForm"
+import StudentForm from "./components/studentForm";
 import { useAuth } from "./components/AuthContext";
+import ChatBox from "./components/ChatBox";
 
 function App() {
   const { token, setToken, email, setEmail, photoData, setPhoto, role, setRole } = useAuth();
@@ -139,80 +137,44 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Routes>
-          
-          <Route path="/" element={<HomePage />} />
+          <Route path='/' element={<HomePage />} />
 
           {/* Define routes for authenticated content */}
+          <Route path='/login' element={<Login updateToken={updateToken} updateEmail={updateEmail} updatePhoto={updatePhoto} updateRole={updateRole} />} />
+          <Route path='/user' element={<UserInfo token={token} email={email} photoData={photoData} />} />
+          <Route path='/chat-box' element={<ChatBox />} />
+          <Route path='/marks' element={<MarksChart token={token} />} />
           <Route
-            path="/login"
-            element={
-              <Login
-                updateToken={updateToken}
-                updateEmail={updateEmail}
-                updatePhoto={updatePhoto}
-                updateRole={updateRole}
-              />
-            }
-          />
-          <Route
-            path="/user"
-            element={<UserInfo token={token} email={email} photoData={photoData} />}
-          />
-          <Route
-            path="/marks"
-            element={<MarksChart token={token} />}
-          />
-          <Route
-            path="/dashboard/*"
+            path='/dashboard/*'
             element={
               token ? (
                 role === "admin" ? (
                   <AdminDashboard>
-                    <Link to="/admininfo">Admin Info</Link> {/* Include the Admin Info link */}
+                    <Link to='/admininfo'>Admin Info</Link> {/* Include the Admin Info link */}
                   </AdminDashboard>
                 ) : (
                   <Dashboard>
-                    <Link to="/ChatSystem">Chat System</Link>
-                    <Link to="/achievements">View Achievements</Link>
-                    <Link to="/progressreport">View Progress Report</Link>
+                    <Link to='/ChatSystem'>Chat System</Link>
+                    <Link to='/achievements'>View Achievements</Link>
+                    <Link to='/progressreport'>View Progress Report</Link>
                   </Dashboard>
                 )
               ) : (
-                <Navigate to="/login" />
+                <Navigate to='/login' />
               )
             }
           />
-          <Route
-            path="/dashboard/ChatSystem"
-            element={<ChatSystem />}
-          />
-          <Route
-            path="/dashboard/achievements"
-            element={<Achievements />}
-          />
-          <Route
-            path="/dashboard/progressreport"
-            element={<MarksChart token={token} />}
-          />
-          <Route
-            path="/admininfo"
-            element={
-              role === "admin" ? (
-                <AdminInfo token={token} email={email} photoData={photoData} />
-              ) : (
-                <Navigate to="/dashboard/*" />
-              )
-            }
-          />
-          <Route path="/admindashboard/*" element={<AdminDashboard />} />
+          <Route path='/dashboard/ChatSystem' element={<ChatSystem />} />
+          <Route path='/dashboard/achievements' element={<Achievements />} />
+          <Route path='/dashboard/progressreport' element={<MarksChart token={token} />} />
+          <Route path='/admininfo' element={role === "admin" ? <AdminInfo token={token} email={email} photoData={photoData} /> : <Navigate to='/dashboard/*' />} />
+          <Route path='/admindashboard/*' element={<AdminDashboard />} />
         </Routes>
       </div>
     </Router>
   );
 }
- 
 
 export default App;
-
---
+++ frontend/src/components/ChatBox.js
@@ -0,0 +1,238 @@
+// lib
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+import socketIO from "socket.io-client";
+
+import ScrollableFeed from "react-scrollable-feed";
+
+import Message from "./Message";
+import "./dashboard.css";
+
+const ENDPOINT = "http://localhost:5050";
+
+const Chat = styled.section`
+  transform: translateX(-50%);
+  margin-left: 50%;
+  width: 90%;
+  background: white;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+`;
+const ChatBoxContent = styled.main`
+  padding: 0.25rem;
+  height: 20rem;
+  /* overflow-y: scroll; */
+`;
+// const IncommingMessage = styled.div`
+//   padding: 0.15rem;
+//   margin: 0.5rem 0rem;
+//   background: lightgrey;
+//   max-width: 20%;
+//   transform: translateX(350%);
+// `;
+// const OutGoingMessage = styled.div`
+//   padding: 0.15rem;
+//   background: lightgrey;
+//   max-width: 20%;
+// `;
+const ChatField = styled.input`
+  width: 85%;
+  font-size: 1rem;
+  @media (max-width: 960px) {
+    margin-top: 1rem;
+    width: 100%;
+  }
+
+  &:focus {
+    border: 1px solid #440ccc;
+  }
+
+  background: rgb(249, 249, 249);
+  outline: none;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+  padding: 0.15rem;
+`;
+
+const PrimaryBtn = styled.button`
+  width: 15%;
+  background: #440ccc;
+  color: white;
+  border: none;
+  outline: none;
+  font-size: 1.25rem;
+  /* height: 1; */
+  cursor: pointer;
+  &:hover {
+    background: #3606a7;
+  }
+
+  @media (max-width: 960px) {
+    margin-top: 1rem;
+    width: 100%;
+  }
+`;
+
+const SendSection = styled.div`
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+
+  padding: 0.5rem 0rem;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+`;
+const socket = socketIO(ENDPOINT, { transports: ["websocket"] });
+
+const ChatBox = () => {
+  // ?STATE
+  const [name, setname] = useState("");
+
+  const [id, setId] = useState("");
+  const [messageToSend, setMessageToSend] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // ? SOCKET
+    // !when circuit is established
+    socket.on("connect", () => {
+      console.log("connected to socket-server from client");
+      setId(socket.id);
+    });
+    //! message from admin after successful circuit establishment
+    socket.on("welcome", (data) => {
+      // @ts-ignore
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+    // !broadcast that , a new user has joined the chat to all other people except that new user
+    socket.on("userJoined", (data) => {
+      // @ts-ignore
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+    //! broadcast that , a  user has left the chat to all other
+    socket.on("leave", (data) => {
+      // @ts-ignore
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    return () => {
+      //  @ts-ignore
+      socket.off();
+    };
+  }, []);
+
+  // ? join btn handler
+  // const joinUser = () => {
+  //   socket.emit('joined', { myUser: name });
+  // };
+
+  // change state of message to send on change of input
+  const updateMessage = (e) => {
+    // @ts-ignore
+    setMessageToSend(e.target.value);
+  };
+  // send message to socket server
+  const sendMessage = () => {
+    //! send message to socket server
+    socket.emit("message", { message: messageToSend, id });
+    // ? blank the message after sending
+    setMessageToSend("");
+  };
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      // @ts-ignore
+      setMessages([...messages, data]);
+      console.log(data.user);
+      console.log(data.message);
+      console.log(data.id);
+    });
+
+    return () => {
+      socket.off();
+    };
+  }, [messages]);
+
+  // if (loggedInUser !== null) {
+  //   return <div style={{ display: 'none' }}>do not show this</div>;
+  // }
+  return (
+    <Chat>
+      {/* <mark>{name}</mark> */}
+      {/* <br /> */}
+
+      {/* <input
+        type="text"
+        id="joinInput"
+        placeholder="Enter Your Name"
+        onChange={(e) => {
+          setname(e.target.value);
+        }}
+      /> */}
+      {/* <button type="button" onClick={joinUser}>
+        JOIN
+      </button> */}
+      <header>
+        <h2
+          className='dashboard_subtitle poppins_medium_500'
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Chat Anyone, Anywhere !
+        </h2>
+      </header>
+      <ChatBoxContent>
+        <ScrollableFeed>
+          <div>
+            <div className='MessageContainer'>
+              <div className={`MessageContainer__message--left`}>
+                <span className='MessageContainer__message--user'>Admin</span>
+                <br />
+
+                <span className='poppins_regular_400'>Welcome to our chat.Here you can chat with all our staffs & customers</span>
+              </div>
+            </div>
+          </div>
+          {/* //todo: messageObj contains user,message,id  */}
+          {messages.map((messageObj, index) => {
+            return (
+              // eslint-disable-next-line react/no-array-index-key
+              <div key={index}>
+                <Message user={messageObj.id === id ? "You" : messageObj.user} message={messageObj.message} classs={messageObj.id === id ? "right" : "left"} />
+              </div>
+            );
+          })}
+        </ScrollableFeed>
+      </ChatBoxContent>
+
+      <SendSection>
+        <ChatField
+          type='text'
+          onChange={(e) => {
+            updateMessage(e);
+          }}
+          onKeyPress={(e) => {
+            // eslint-disable-next-line no-unused-expressions
+            e.key === "Enter" ? sendMessage() : null;
+          }}
+          value={messageToSend}
+        />
+        <PrimaryBtn className='poppins_regular_400' onClick={sendMessage}>
+          send
+        </PrimaryBtn>
+      </SendSection>
+    </Chat>
+  );
+};
+
+export default ChatBox;
